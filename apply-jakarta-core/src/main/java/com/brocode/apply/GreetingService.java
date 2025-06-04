e2b21e4d9f8d2cc0fc471df79690170e95a4968f
tomas <gandalf@lord-of-the-lines.com>
2025-06-04 21:48:20 +0200

    feat(jakarta): fixed code inspections

--- apply-jakarta-core/src/main/java/com/brocode/apply/GreetingService.java
+++ apply-jakarta-core/src/main/java/com/brocode/apply/GreetingService.java
@@ -2,11 +2,8 @@ package com.brocode.apply;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.UriInfo;
 
 @Path("/")
 public class GreetingService {
