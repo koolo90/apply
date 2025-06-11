dcf9a319f80b2e11fc48a9aa8385b7e32580600a
tomas <gandalf@lord-of-the-lines.com>
2025-06-11 09:11:10 +0200

    test(core): fixed assertions in hello api tests

--- apply-spring-core/src/test/java/com/brocode/apply/test/integration/HelloApiTests.java
+++ apply-spring-core/src/test/java/com/brocode/apply/test/integration/HelloApiTests.java
@@ -1,9 +1,11 @@
 package com.brocode.apply.test.integration;
 
+import com.brocode.apply.buissness.model.Candidate;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(classes = {RestClientConfig.class})
@@ -13,8 +15,12 @@ class HelloApiTests {
 
     @Test
     void helloApiTest(){
-        String formatted = "http://localhost:%d/hello".formatted(8080);
-        String forObject = this.restTemplate.getForObject(formatted, String.class);
-        Assertions.assertThat(forObject).isEqualTo("Hello World");
+        String formatted = "http://localhost:%d/apply-spring/hello/global".formatted(8080);
+
+        ResponseEntity<Candidate> candidateResponseEntity = this.restTemplate.getForEntity(formatted, Candidate.class);
+        Candidate responseCandidate = candidateResponseEntity.getBody();
+
+        Assertions.assertThat(responseCandidate.getEmail()).isNotBlank().isEqualTo("world@example.com");
+        Assertions.assertThat(responseCandidate.getUsername()).isNotBlank().isEqualTo("World");
     }
 }
