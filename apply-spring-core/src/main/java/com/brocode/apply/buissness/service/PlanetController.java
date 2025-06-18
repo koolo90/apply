2d391bcdef5591ed2ea09b9ea53487a82e820462
tomas <gandalf@lord-of-the-lines.com>
2025-06-18 15:23:48 +0200

    feat(core): added new controller

---
+++ apply-spring-core/src/main/java/com/brocode/apply/buissness/service/PlanetController.java
@@ -0,0 +1,49 @@
+package com.brocode.apply.buissness.service;
+
+import com.brocode.apply.buissness.model.Candidate;
+import com.brocode.apply.repositories.CandidateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * <h1>Based on: <a href="https://www.baeldung.com/spring-injection-lombok">Constructor Injection in Spring with Lombok</a></a></h1>
+ * <h3>Credits:</h3>
+ *      <h4>Written by:baeldung</h4>
+ *      <h4>Reviewed by:Grzegorz Piwowarek</h4>
+ * <p>The onConstructor parameter accepts an array of annotations (or a single annotation like in this specific example)
+ * that are to be put on a generated constructor. The double underscore idiom has been introduced because of the
+ * backward compatibility issues.</p>
+ * <h3>According to the documentation:</h3>
+ * <em>The reason of the weird syntax is to make this feature work in javac 7 compilers; the @__ type is an annotation
+ * reference to the annotation type __ (double underscore) which doesn’t actually exist; this makes javac 7 delay
+ * aborting the compilation process due to an error because it is possible an annotation processor will later create
+ * the __ type.</em>
+ * <p>And others aren't documented.</p>
+ */
+@RestController
+@RequestMapping("/planet")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class PlanetController {
+    private final CandidateRepository candidateRepository;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Candidate> getAll() {
+        return candidateRepository.findAll();
+    }
+
+    @PostMapping(value = "/new", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Candidate> createNew(@RequestBody Candidate candidate) {
+        return ResponseEntity.of(Optional.of(candidateRepository.save(candidate)));
+    }
+
+    @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Candidate> getByName(@PathVariable String username) {
+        return ResponseEntity.of(candidateRepository.findByUsername(username));
+    }
+}
