f7e2f361b1a1496e96f6a63addc8f944085b813d
tomas <gandalf@lord-of-the-lines.com>
2025-07-01 16:00:42 +0200

    feat(core): removed unused class

--- apply-spring-core/src/main/java/com/brocode/apply/repositories/CustomCandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brocode.apply.repositories;
-
-import com.brocode.apply.buissness.model.CelestialBody;
-
-import java.util.Optional;
-
-public interface CustomCandidateRepository {
-    Optional<CelestialBody> deleteByUsername(String username);
-}
